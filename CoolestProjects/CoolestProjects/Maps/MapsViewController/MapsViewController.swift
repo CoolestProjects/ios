879c6b5001464c659c33f94cf0ed82c845149c2c
Rich <rtoolan91@gmail.com>
2016-06-07 07:09:33 +0100

    Initial map work

--- CoolestProjects/CoolestProjects/Maps/MapsViewController/MapsViewController.swift
+++ CoolestProjects/CoolestProjects/Maps/MapsViewController/MapsViewController.swift
@@ -9,8 +9,35 @@
 import UIKit
 
 class MapsViewController: BaseViewController {
+    @IBOutlet weak var collectionView : UICollectionView?
+    
+    var mapModel = MapsModel(data: [("Car Park", "https://pbs.twimg.com/media/CkSWu6RWgAAwcoY.jpg:large"), ("Other Place", "https://pbs.twimg.com/media/CkSWu6RWgAAwcoY.jpg:large")])
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Maps"
+        self.title = NSLocalizedString("Maps", comment: "Maps")
+        let nibName = UINib(nibName: "MapViewCollectionViewCell", bundle:nil)
+        
+        self.collectionView?.registerNib(nibName, forCellWithReuseIdentifier: "cell")
+    }
+}
+
+extension MapsViewController : UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mapModel.numberOfItems;
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let item = mapModel.item(atIndex: indexPath.row)! as (String, String)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! MapViewCollectionViewCell
+        cell.displayModel(item)
+        
+        return cell
+    }
+}
+
+extension MapsViewController : UICollectionViewDelegate {
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+
+        
     }
 }
