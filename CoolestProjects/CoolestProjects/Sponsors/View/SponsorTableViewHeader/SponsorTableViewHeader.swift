e24b02cf37e3072ef3c0248da3197e6ed0969b17
Colin Pierse <colin.pierse@mttnow.com>
2016-06-10 09:25:28 +0100

    *Sponsor View. Its not 100%.

--- CoolestProjects/CoolestProjects/Sponsors/View/SponsorTableViewHeader/SponsorTableViewHeader.swift
+++ CoolestProjects/CoolestProjects/Sponsors/View/SponsorTableViewHeader/SponsorTableViewHeader.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class SponsorTableViewHeader: UITableViewHeaderFooterView {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    var titleLabel: UILabel!
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setupTitleLabel()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setupTitleLabel() {
+        
+        let label = UILabel()
+        label.font = AppFonts.tableHeaderTitle
+        label.textAlignment = .Center
+        self.contentView.addSubview(label);
+        
+        self.titleLabel = label
+    
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraintEqualToAnchor(self.contentView.topAnchor).active = true
+        label.bottomAnchor.constraintEqualToAnchor(self.contentView.bottomAnchor).active = true
+        label.leadingAnchor.constraintEqualToAnchor(self.contentView.leadingAnchor).active = true
+        label.trailingAnchor.constraintEqualToAnchor(self.contentView.trailingAnchor).active = true
+    }
     
     func displayViewModel(viewModel: SponsorTrierViewModel) {
         self.titleLabel.text = viewModel.tierName
+        self.contentView.backgroundColor = viewModel.tierColor
     }
 
 }
