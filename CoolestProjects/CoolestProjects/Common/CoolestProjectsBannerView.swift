dfae21fc12bcd79d30be8eab9ecb9f83dfa3b86c
Natasha Cole <natasha.cole@mttnow.com>
2017-05-20 16:47:44 +0100

    New generic banner for all views. New UI.

---
+++ CoolestProjects/CoolestProjects/Common/CoolestProjectsBannerView.swift
@@ -0,0 +1,14 @@
+//
+//  CoolestProjectsBannerView.swift
+//  CoolestProjects
+//
+//  Created by Natasha Cole on 20/05/2017.
+//  Copyright © 2017 coderdojo. All rights reserved.
+//
+
+import UIKit
+
+class CoolestProjectsBannerView: UIView {
+
+
+}
