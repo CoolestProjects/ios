83d14c70d85b4b77db54a1ed8da6e085facbffa5
booranger <boomanchew@gmail.com>
2017-05-22 21:46:13 +0100

    Updated sponsor box

--- CoolestProjects/CoolestProjects/Home/View/SponsorBoxTableViewCell/SponsorBoxTableViewCell.swift
+++ CoolestProjects/CoolestProjects/Home/View/SponsorBoxTableViewCell/SponsorBoxTableViewCell.swift
@@ -13,20 +13,39 @@ class SponsorBoxTableViewCell : UITableViewCell, Configurable {
     typealias ItemType = SponsorBox
     
     @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var stackView: UIStackView!
-    
+    @IBOutlet weak var diamondStackView: UIStackView!
+    @IBOutlet weak var platinumStackView: UIStackView!
+    @IBOutlet weak var platinumLowerStackView: UIStackView!
+    @IBOutlet weak var goldStackView: UIStackView!
+
     func configure(with item: ItemType) {
         title.text = item.title
-        title.textColor = item.textColor
-            
-        for image in item.images {
-            stackView.addArrangedSubview(UIImageView(image: image))
-        }                
+        
+        for image in item.diamondImages {
+            diamondStackView.addArrangedSubview(UIImageView(image: image))
+        }
+        for image in item.platinumHigherImages {
+            platinumStackView.addArrangedSubview(UIImageView(image: image))
+        }
+        for image in item.platinumLowerImages {
+            platinumLowerStackView.addArrangedSubview(UIImageView(image: image))
+        }
+        for image in item.goldImages {
+            goldStackView.addArrangedSubview(UIImageView(image: image))
+        }
     }
     
     override func prepareForReuse() {
+        // TODO: check the performace of this. Seems very wasteful. 
+        self.removeViewsFrom(stackView: diamondStackView)
+        self.removeViewsFrom(stackView: platinumStackView)
+        self.removeViewsFrom(stackView: platinumLowerStackView)
+        self.removeViewsFrom(stackView: goldStackView)
+
+    }
+    
+    func removeViewsFrom( stackView: UIStackView) {
         for view in stackView.arrangedSubviews {
-            stackView.removeArrangedSubview(view)
             view.removeFromSuperview()
         }
     }
