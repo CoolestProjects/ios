181623073c3b8f6d42e4a4334d8e29613c6ad821
Natasha Cole <natasha.cole@mttnow.com>
2017-05-23 17:53:16 +0100

    Removal of segment reference.

--- CoolestProjects/CoolestProjects/About/AboutContainerViewController.swift
+++ CoolestProjects/CoolestProjects/About/AboutContainerViewController.swift
@@ -14,8 +14,7 @@ let aboutIndex = 0
 let disclaimerIndex = 1
 
 class AboutContainerViewController : BaseViewController {
-    
-    @IBOutlet weak var aboutSectionsSegmentedControl: UISegmentedControl!
+  
     @IBOutlet weak var aboutContainerView: UIView!
     @IBOutlet weak var disclaimerContainerView: UIView!
     
@@ -26,40 +25,13 @@ class AboutContainerViewController : BaseViewController {
     
     func setupUI() {
         setupNavigationBar()
-        setupAboutSectionsSegmentedControl()
     }
     
     override func setupNavigationBar() {
         title = NSLocalizedString("title", tableName: "About", comment: "")
         super.setupNavigationBar()
     }
-    
-    func setupAboutSectionsSegmentedControl() {
-        
-        aboutSectionsSegmentedControl.setTitle(NSLocalizedString("section.about", tableName: "About", comment: ""), forSegmentAt: aboutIndex)
-        aboutSectionsSegmentedControl.setTitle(NSLocalizedString("section.disclaimer", tableName: "About", comment: ""), forSegmentAt: disclaimerIndex)
-        
-        let normalStateAttributes = [
-            NSFontAttributeName: AppFonts.tabBarFontSelected,
-            NSForegroundColorAttributeName: AppColors.darkTextColor
-        ]
-        
-        let selectedStateAttributes = [
-            NSFontAttributeName: AppFonts.tabBarFontSelected,
-            NSForegroundColorAttributeName: AppColors.darkTextColor
-        ]
-                            
-        aboutSectionsSegmentedControl.setTitleTextAttributes(normalStateAttributes, for: .normal)
-        aboutSectionsSegmentedControl.setTitleTextAttributes(selectedStateAttributes, for: .selected)
-        
-        aboutSectionsSegmentedControl.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
-        aboutSectionsSegmentedControl.setDividerImage(UIImage(), forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
-        aboutSectionsSegmentedControl.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .selected, barMetrics: .default)
-        
-        aboutSectionsSegmentedControl.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
-        aboutSectionsSegmentedControl.setBackgroundImage(UIImage(), for: .selected, barMetrics: .default)                        
-    }
-    
+  
     @IBAction func changeAboutSection(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == aboutIndex {
             showAboutContainer()
