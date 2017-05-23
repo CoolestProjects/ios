cc22d3fabfe3dc14cb8b37184743b194f23adea6
Natasha Cole <natasha.cole@mttnow.com>
2017-05-23 20:02:58 +0100

    New XIB for about. New reference on story board. Copy cat logic from home screen view in order to get the table in place.

--- CoolestProjects/CoolestProjects/About/AboutViewController.swift
+++ CoolestProjects/CoolestProjects/About/AboutViewController.swift
@@ -8,27 +8,121 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class AboutViewController : UIViewController {
+class AboutViewController : BaseViewController {
     
-    @IBOutlet weak var pageHeaderView: PageHeaderView!
-    @IBOutlet weak var webView: UIWebView!
-        
+    @IBOutlet weak var overlayView: UIView!
+    @IBOutlet weak var wrapperView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+  
+    let tableHeaderView = PageHeaderView.pageHeaderView()!
+    let tableFooterView = HomeTableFooterView.footerView()!
+    let viewModel = HomeViewModel()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadHeaderView()
-        loadAboutContent()
+        setupUI()
     }
-    
+  
+    override func viewDidLayoutSubviews() {
+      super.viewDidLayoutSubviews()
+      forceTableViewLayoutPhase()
+      updateTableHeaderFrame()
+      updateTableFooterFrame()
+    }
+  
+    func setupUI() {
+      setupBackground()
+      setupNavigationBar()
+      setupTable()
+    }
+  
+    func setupBackground() {
+      overlayView.backgroundColor = UIColor.red
+      wrapperView.backgroundColor = UIColor.black
+    }
+  
+    override func setupNavigationBar() {
+      navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+      navigationController?.navigationBar.shadowImage = UIImage()
+      navigationItem.titleView = UIImageView(image: UIImage(named: "coolest-project-logo"))
+    }
+  
+    func setupTable() {
+      tableView.tableHeaderView = tableHeaderView
+      tableView.tableFooterView = tableFooterView
+      tableView.estimatedRowHeight = 300.0;
+      tableView.rowHeight = UITableViewAutomaticDimension;
+      tableView.register(UINib.init(nibName: "SponsorBoxTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "sponsorBox")
+      tableView.register(PageHeaderTableViewCell.self, forCellReuseIdentifier: "pageHeader")
+      tableView.register(UINib.init(nibName: "ContentTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "contentCell")
+      tableView.register(UINib.init(nibName: "BlurbTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "blurbBox")
+    }
+  
+    func forceTableViewLayoutPhase() {
+      tableView.setNeedsLayout()
+      tableView.layoutIfNeeded()
+    }
+  
+    func updateTableHeaderFrame() {
+      let minTableHeaderViewHeight = calculateMinTableHeaderViewHeight()
+      let availableSpace = tableView.bounds.height - SponsorBoxHeight
+      tableHeaderView.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: max(minTableHeaderViewHeight, availableSpace))
+      tableView.tableHeaderView = tableHeaderView
+    }
+  
+    func updateTableFooterFrame() {
+      tableFooterView.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 100.0)
+      tableView.tableFooterView = tableFooterView
+    }
+  
+    func calculateMinTableHeaderViewHeight() -> CGFloat {
+      tableHeaderView.frame = CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 1600.0)
+      tableHeaderView.setNeedsLayout()
+      tableHeaderView.layoutIfNeeded()
+      return tableHeaderView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height
+    }
+  
     func loadAboutContent() {
         if let aboutURL = Bundle.main.url(forResource: "about", withExtension: "html") {
             let request = URLRequest(url: aboutURL)
-            webView.loadRequest(request)
+           // webView.loadRequest(request)
         }
     }
+  }
+extension AboutViewController : UITableViewDataSource {
   
-    func loadHeaderView() {
-      pageHeaderView = Bundle.main.loadNibNamed("PageHeaderView", owner: nil, options: nil)?.first as? PageHeaderView
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.tableViewData.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let component = viewModel.tableViewData[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: component.componentIdentifier, for: indexPath)
+    
+    // TODO: try to improve using better generics or protocol oriented data sources
+    
+    if (component is SponsorBox) {
+      let sponsorBox = component as! SponsorBox
+      let sponsorBoxCell = cell as! SponsorBoxTableViewCell
+      sponsorBoxCell.configure(with: sponsorBox)
     }
+    
+    if (component is ContentViewModel) {
+      let content = component as! ContentViewModel
+      let contentCell = cell as! ContentTableViewCell
+      contentCell.configure(with: content);
+    }
+    
+    if (component is BlurbBox) {
+      let blurbBox = component as! BlurbBox
+      let contentCell = cell as! BlurbTableViewCell
+      contentCell.configure(with: blurbBox);
+    }
+    
+    return cell
+  }
   
 }
+
