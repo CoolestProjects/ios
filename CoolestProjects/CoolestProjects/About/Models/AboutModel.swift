3ccc07d898c7b433cc927782a2da03ef3984da3c
Natasha Cole <natasha.cole@mttnow.com>
2017-05-28 18:25:31 +0100

    URL consumption.

--- CoolestProjects/CoolestProjects/About/Models/AboutModel.swift
+++ CoolestProjects/CoolestProjects/About/Models/AboutModel.swift
@@ -12,10 +12,11 @@ struct AboutModel: Component {
   
     var componentIdentifier: String = "AboutViewCell"
     var title: String
-    var mapUrl: String
-    init(title: String, mapUrl: String) {
-        self.title = title
-        self.mapUrl = mapUrl
+    var aboutContentURL: String
+  
+    init(title: String, aboutContentURL: String) {
+      self.title = title
+      self.aboutContentURL = aboutContentURL
     }
 }
 
@@ -25,19 +26,19 @@ class AboutViewModel {
     let title: String
     
     init() {
-
-        title = "About"
-        loadAboutContent()
-        tableViewData = [
-            BlurbBox(title: NSLocalizedString("date.tilte", tableName: "Home", comment: "")),
-            AboutModel(title: NSLocalizedString("About", tableName: "Maps", comment: ""), mapUrl: "https://firebasestorage.googleapis.com/v0/b/coolestprojectsapp.appspot.com/o/Maps%2Fcoolestprojectsmap.png?alt=media&token=cf6515dd-9947-4595-a203-422c6427a2ca")
+      title = "About"
+      tableViewData = [
+        BlurbBox(title: NSLocalizedString("date.tilte", tableName: "Home", comment: "")),
+        AboutModel(title: NSLocalizedString("About", tableName: "About", comment: ""), aboutContentURL: loadAboutContent())
         ]
     }
-  
-    func loadAboutContent() {
+
+    func loadAboutContent() -> String {
       if let aboutURL = Bundle.main.url(forResource: "about", withExtension: "html") {
-        let request = URLRequest(url: aboutURL)
-       // webView.loadRequest(request)
+      let path:String = aboutURL.path
+        return path
       }
+      //TO DO: Add default.
+      return ""
     }
 }
