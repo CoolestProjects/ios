4fbe5d4254795cb2de5e7430ce80b28e9c52fca7
Valentino Gattuso <valentino.gattuso@mttnow.com>
2017-06-15 01:10:59 +0100

    Revert "Adding analytics that dont compile"
    This reverts commit 0a69172b9851bf01266e5f2afd01e6c05e9a1d51.

--- CoolestProjects/CoolestProjects/AppDelegate.h
+++ CoolestProjects/CoolestProjects/AppDelegate.h
@@ -7,28 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <EstimoteSDK/EstimoteSDK.h>
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (nonatomic) ESTBeaconManager *beaconManager;
+
 
 @end
 
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // 3. Set up your Estimote Cloud credentials
-    [ESTConfig setupAppID:@"coolest-projects-782" andAppToken:@"bc1d0eea387420e59c6480bd3c7ee1b4"];
-    
-    // 4. Enable analytics
-    [ESTConfig enableRangingAnalytics:YES];
-    [ESTConfig enableMonitoringAnalytics:YES];
-    
-    // 5. Instantiate the beacon manager
-    self.beaconManager = [ESTBeaconManager new];
-    
-    // 6. Start scanning for beacons
-    [self.beaconManager startRangingBeaconsInRegion:
-     [[CLBeaconRegion alloc] initWithProximityUUID:@"0B691DC7-DC66-4C12-B197-CC8F56735C0C"
-                                        identifier:@"cp beacons"]];
